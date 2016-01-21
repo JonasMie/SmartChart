a1e42e6d3f883c7d83d1fdb85ad261331a541257
Jonas Miederer <jm074>
2016-01-21 13:47:51 +0100

    multi colored chart

--- learning/tree/decisionTree.py
+++ learning/tree/decisionTree.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn import tree
 from sklearn.ensemble import ExtraTreesClassifier
 
+import utils
+
 
 def train(data, target):
     clf = tree.DecisionTreeClassifier()
@@ -19,25 +21,49 @@ def tree_feat_sel(X, y, feature_names, threshold=None, plot=True):
                                   random_state=0)
     forest.fit(X, y)
     features = []
+    all_features = []
     importances = forest.feature_importances_
     indices = np.argsort(importances)[::-1]
+
+    metadata_std= []
+    metadata_imp = []
+    metadata_ind = []
+
+    mir_std = []
+    mir_imp = []
+    mir_ind = []
+
+    std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+                 axis=0)
+
     print("Feature ranking:")
     for f in range(X.shape[1]):
+        all_features.append(feature_names[indices[f]])
+        if feature_names[indices[f]] in utils.mir:
+            mir_std.append(std[indices[f]])
+            mir_imp.append(importances[importances[f]])
+            mir_ind.append(indices[f])
+        else:
+            metadata_std.append(std[indices[f]])
+            metadata_imp.append(importances[importances[f]])
+            metadata_ind.append(indices[f])
         print("{}. feature {} ({})".format(f + 1, feature_names[indices[f]], importances[indices[f]]))
-        if threshold and importances[indices[f]]> threshold:
+        if threshold and importances[indices[f]] > threshold:
             features.append(feature_names[indices[f]])
 
     if plot:
-        std = np.std([tree.feature_importances_ for tree in forest.estimators_],
-                     axis=0)
 
         # Plot the feature importances of the forest
         plt.figure()
         plt.title("Feature importances")
-        plt.bar(range(X.shape[1]), importances[indices],
-                color="r", yerr=std[indices], align="center")
-        plt.xticks(range(X.shape[1]), indices)
+        plt.bar(metadata_ind, metadata_imp,
+                color="r", yerr=metadata_std, align="center", width=.5)
+        plt.bar(mir_ind, mir_imp,
+                color="b", align="center", width=.5)
+        plt.xticks(range(X.shape[1]), all_features, rotation='vertical')
+        plt.tick_params(axis='x', labelsize=8)
         plt.xlim([-1, X.shape[1]])
+
         plt.show()
 
     if threshold:
