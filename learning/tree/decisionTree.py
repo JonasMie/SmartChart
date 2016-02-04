214fdb900392ba514dec500592a2a67c315b20e5
Jonas Miederer <jonasmied@gmail.com>
2016-02-04 23:52:57 +0100

    decisioi tree

--- learning/tree/decisionTree.py
+++ learning/tree/decisionTree.py
@@ -1,18 +1,22 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import tree
 from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
+
 from learning import learning_utils
 
-def train(data, target):
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(data, target)
-    return clf
+
+def train(conf):
+    training_data, training_targets = learning_utils.getData(conf['datasets'], type=conf['type'], split=True,
+                                                             balanced=conf['balanced'], shuffle=True)
+
+    clf = tree.DecisionTreeClassifier(criterion=conf['criterion'], splitter='best')
+    pipeline = learning_utils.getPipeline(training_data, clf, 'decision_tree')
+    return pipeline.fit(training_data, training_targets), training_data.columns.values
 
 
-def predict(clf, data):
-    return clf.predict_proba(data)
+def predict(trackName, artistName, track_id, clf):
+    learning_utils.predict(trackName, artistName, track_id, clf)
 
 
 def tree_feat_sel(X, y, feature_names, type, trees=None, threshold=None, plot=True):
@@ -38,10 +42,11 @@ def tree_feat_sel(X, y, feature_names, type, trees=None, threshold=None, plot=Tr
 
     if plot:
         std = np.std([tree.feature_importances_ for tree in clf.estimators_],
-                         axis=0)
+                     axis=0)
         # utils.plot_chart(clf.__class__.__name__, trees, importances, indices, ordered_features, std, X.shape[1],type)
         # utils.plot_chart_h(clf.__class__.__name__, trees, importances, indices, ordered_features, std, X.shape[1],type)
-        learning_utils.plot_pie(clf.__class__.__name__, trees, importances, indices, ordered_features, threshold, X.shape[1], type)
+        learning_utils.plot_pie(clf.__class__.__name__, trees, importances, indices, ordered_features, threshold,
+                                X.shape[1], type)
 
     if threshold:
         return features
